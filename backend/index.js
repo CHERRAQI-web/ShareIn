b7df83ee25e36da93f506042e7e31a3e7e0ad883
CHERRAQI <aziza.cherraqi@gmail.com>
2025-10-14 19:40:59 +0100

    change

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,4 @@ app.use("/api/auth", authRoutes);
 
 
 // Démarapp.use('/api', studentsRouter);rer le serveur
-const port = process.env.PORT ?? 5000;
-app.listen(port, () => {
-  console.log(` Serveur lancé sur http://localhost:${port}`);
-});
+export default app;
